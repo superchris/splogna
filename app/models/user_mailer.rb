00b948803d618ad546a22c70978f65b90b502ab6
Chris Nelson <me@christophernelsonconsulting.com>
2009-04-15 23:17:54 -0400

    improved user email

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,21 +3,21 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://www.splogna.com/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://www.splogna.com/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "admin"
+      @subject     = "[Splogna] "
       @sent_on     = Time.now
       @body[:user] = user
     end
