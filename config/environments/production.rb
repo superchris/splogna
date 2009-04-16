b83d2d5230a7fdc95a2763a34067e690c9d9d2f1
Chris Nelson <me@christophernelsonconsulting.com>
2009-04-15 22:59:59 -0400

    forgot stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ config.action_view.cache_template_loading            = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host = "http://assets.example.com"
 
-config.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :sendmail
 
 config.action_mailer.perform_deliveries = true
 
