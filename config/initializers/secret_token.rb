5e5ec7579f1e7ab93d64f9932f3ffe4c21348001
Chris Nelson <me@christophernelsonconsulting.com>
2010-09-20 10:39:07 -0400

    ran rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splogna::Application.config.secret_token = 'c84a81bd46775106d87839d40764e3769277c83c86eedc13026fc01765409e692c0cbb5fdd3a5de328aff57ee8d944c8dbd7eb037d363222c3b31e7d0f8fa76c'
