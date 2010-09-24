2540e47685457ce91590a77bc69a56d713d9fb31
Chris Nelson <me@christophernelsonconsulting.com>
2010-09-24 15:53:38 -0400

    refactored duplicate code into Searchable

--- app/models/got.rb
+++ app/models/got.rb
@@ -5,8 +5,5 @@ class Got < ActiveRecord::Base
 
   acts_as_taggable
 
-  def self.search(term)
-    query_term = "%#{term}%"
-    where(Got[:title].matches(query_term).or(Got[:description].matches(query_term)))
-  end
+  extend Searchable
 end
--- app/models/need.rb
+++ app/models/need.rb
@@ -9,9 +9,5 @@ class Need < ActiveRecord::Base
   
   acts_as_taggable
 
-  def self.search(term)
-    query_term = "%#{term}%"
-    find(:all, :conditions => ["title like ? or description like ?",
-      query_term, query_term])
-  end
+  extend Searchable
 end
