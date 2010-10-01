f7154342072f548ae9f11b0399319770d52a2f64
Doug Alcorn <dougalcorn@gmail.com>
2010-09-30 22:26:55 -0400

    upgraded routes and whack public/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,36 @@
 Splogna::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  resources :messages
+  match '/logout' => 'sessions#destroy', :as => :logout
+  match '/login' => 'sessions#new', :as => :login
+  match '/register' => 'users#create', :as => :register
+  match '/signup' => 'users#new', :as => :signup
+  match '/activate/:activation_code' => 'users#activate', :as => :activate
+  resources :users
+  resource :session
+  match '/' => 'splogna#index'
+  resources :needs
+  resources :gots
+  resources :tags
+  match '/:controller(/:action(/:id))'
 end
+
+# ActionController::Routing::Routes.draw do |map|
+#   map.resources :messages
+
+#   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+#   map.login '/login', :controller => 'sessions', :action => 'new'
+#   map.register '/register', :controller => 'users', :action => 'create'
+#   map.signup '/signup', :controller => 'users', :action => 'new'
+#   map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
+  
+#   map.resources :users
+
+#   map.resource :session
+
+#   map.root :controller => "splogna"
+
+#   map.resources :needs, :gots, :tags
+
+#   map.connect ':controller/:action/:id'
+#   map.connect ':controller/:action/:id.:format'
+# end
